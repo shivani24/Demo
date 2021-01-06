9068bd8d3db246af55517d5d348a1b4fb61e91df
shivani24 <31152564+shivani24@users.noreply.github.com>
2021-01-06 10:44:28 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi and Hello, Shivani! How are u doing with this project sdm sdjcadc!!!!!!!!!!!"); 
+        System.out.println("Hi and Hello, Thanks Shivani! How are u doing with this project sdm sdjcadc!!!!!!!!!!!"); 
     }
 }
 

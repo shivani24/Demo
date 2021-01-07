d7a8597ddb631edd89b87081bf06b0c5f96e7fb4
shivani24 <31152564+shivani24@users.noreply.github.com>
2021-01-06 22:34:33 -0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("New program Hi and Hello, Thanks Shivani! How are u doing with this project sdm sdjcadc!!!!!!!!!!!"); 
-    }
-}
-
-
